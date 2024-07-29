fec11fbd4b3adf1439c3796214fc7e0cb05b9be2
XXXZZZHHH123 <e1133135@u.nus.edu>
2024-07-29 16:39:16 +0800

    Testandpush

--- src/main/java/sg/nus/iss/javaspring/adprojrct/Controllers/TestController.java
+++ src/main/java/sg/nus/iss/javaspring/adprojrct/Controllers/TestController.java
@@ -1,4 +1,12 @@
 package sg.nus.iss.javaspring.adprojrct.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping(value ="/policy")
+    public String getPolicyPage() {
+        return "policy";
+    }
 }
